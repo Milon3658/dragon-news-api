8ab10772a77cc0fe0ca887014e880c3c444a3012
Milon <jan.milon.56@gmail.com>
2022-10-23 19:45:10 +0600

    category api make

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port =process.env.PORT || 5000;
 
+
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res) => {
     res.send('news caterogy api runnning')
-})
+});
+
+app.get('/categories', (req, res)=>{
+    res.send(categories);
+});
 
 app.listen(port, () => {
     console.log(`news api running on server port, ${port}`);
-})
+});
